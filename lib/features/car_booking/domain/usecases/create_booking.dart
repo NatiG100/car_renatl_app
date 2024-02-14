43781269e122dc1cc9758bb31c0fbfc95a027c22
NatiGK <natnaelgashu2022@gmail.com>
2024-02-14 11:44:27 +0300

    inject dependencies

--- lib/features/car_booking/domain/usecases/create_booking.dart
+++ lib/features/car_booking/domain/usecases/create_booking.dart
@@ -3,7 +3,7 @@ import 'package:car_renatl_app/core/usecases/usecase.dart';
 import 'package:car_renatl_app/features/car_booking/domain/entities/booking.dart';
 import 'package:car_renatl_app/features/car_booking/domain/repositories/booking_repository.dart';
 
-abstract class CreateBookingUseCase extends UseCase<DataState<BookingEntity>, BookingEntity>{
+class CreateBookingUseCase extends UseCase<DataState<BookingEntity>, BookingEntity>{
   final BookingRepository _bookingRepository;
   CreateBookingUseCase(this._bookingRepository);
   
--- lib/features/injection_container.dart
+++ lib/features/injection_container.dart
@@ -10,6 +10,18 @@ import 'package:car_renatl_app/features/auth/presentation/bloc/auth/remote_auth_
 import 'package:car_renatl_app/features/car/data/repository/mock_car_repository.dart';
 import 'package:car_renatl_app/features/car/domain/repository/car_repository.dart';
 import 'package:car_renatl_app/features/car/domain/usecases/get_cars.dart';
+import 'package:car_renatl_app/features/car/domain/usecases/get_saved_cars.dart';
+import 'package:car_renatl_app/features/car/domain/usecases/remove_car.dart';
+import 'package:car_renatl_app/features/car/domain/usecases/remove_cars.dart';
+import 'package:car_renatl_app/features/car/domain/usecases/save_car.dart';
+import 'package:car_renatl_app/features/car/presentation/bloc/local/local_car_bloc.dart';
+import 'package:car_renatl_app/features/car/presentation/bloc/remote/remote_car_bloc.dart';
+import 'package:car_renatl_app/features/car_booking/data/repository/mock_booking_repository.dart';
+import 'package:car_renatl_app/features/car_booking/domain/repositories/booking_repository.dart';
+import 'package:car_renatl_app/features/car_booking/domain/usecases/create_booking.dart';
+import 'package:car_renatl_app/features/car_booking/domain/usecases/get_booking.dart';
+import 'package:car_renatl_app/features/car_booking/domain/usecases/my_bookings.dart';
+import 'package:car_renatl_app/features/car_booking/presentation/bloc/booking_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -44,4 +56,32 @@ Future<void> initializeDependencies() async {
   sl.registerSingleton<CarRepository>(MockCarRepository());
   //use cases
   sl.registerSingleton<GetCarsUseCase>(GetCarsUseCase(sl()));
+  sl.registerSingleton<GetSavedCarsUseCase>(GetSavedCarsUseCase(sl()));
+  sl.registerSingleton<RemoveCarUseCase>(RemoveCarUseCase(sl()));
+  sl.registerSingleton<RemoveCarsUseCase>(RemoveCarsUseCase(sl()));
+  sl.registerSingleton<SaveCarUseCase>(SaveCarUseCase(sl()));
+
+  //inject remote bloc logic
+  sl.registerFactory<LocalCarsBloc>(
+    () => LocalCarsBloc(
+      sl(),
+      sl(),
+      sl(),
+      sl(),
+    ),
+  );
+  //inject local bloc logic
+  sl.registerFactory<RemoteCarsBloc>(() => RemoteCarsBloc(sl()));
+
+  /////////////////// Booking Section ///////////////////////
+  //repo
+  sl.registerSingleton<BookingRepository>(MockBookingRepository());
+
+  //usecase
+  sl.registerSingleton<CreateBookingUseCase>(CreateBookingUseCase(sl()));
+  sl.registerSingleton<GetBookingUseCase>(GetBookingUseCase(sl()));
+  sl.registerSingleton<MyBookingsUseCase>(MyBookingsUseCase(sl()));
+
+  // inject bloc logic
+  sl.registerFactory<BookingBloc>(() => BookingBloc(sl(), sl()));
 }
